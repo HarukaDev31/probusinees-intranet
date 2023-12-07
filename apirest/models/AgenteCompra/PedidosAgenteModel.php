94c13d5808e047f1dfffe2c2d9d4c80d8d812ab9
root <root@DESKTOP-A17R44P>
2023-12-06 23:50:21 -0500

    mail v9

--- apirest/models/AgenteCompra/PedidosAgenteModel.php
+++ apirest/models/AgenteCompra/PedidosAgenteModel.php
@@ -137,7 +137,7 @@ class PedidosAgenteModel extends CI_Model{
     }
 	
 	public function downloadImage($id){
-		$query = "SELECT Txt_Url_Imagen_Producto FROM agente_compra_pedido_cabecera WHERE ID_Pedido_Cabecera = " . $id . " LIMIT 1";
+		$query = "SELECT Txt_Url_Imagen_Producto FROM agente_compra_pedido_detalle WHERE ID_Pedido_Detalle = " . $id . " LIMIT 1";
 		return $this->db->query($query)->row();
 	}
 }
