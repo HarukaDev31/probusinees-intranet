d989b2a0e344cf2f3f07242f21f699a2c3e9dc7a
Haruka31Dev <u202412824@upc.edu.pe>
2024-06-13 15:53:26 -0500

    fix styles

--- apirest/models/CargaConsolidada/CCotizacionesModel.php
+++ apirest/models/CargaConsolidada/CCotizacionesModel.php
@@ -1093,6 +1093,7 @@ class CCotizacionesModel extends CI_Model
     public function generateMassiveExcelPayrolls($objPHPExcel, $tarifas)
     {
         //init more memory
+        $originalMemoryLimit = ini_get('memory_limit');
         ini_set('memory_limit', '512M');
         $this->load->library('PHPExcel');
         $this->load->library('zip');
@@ -1117,6 +1118,8 @@ class CCotizacionesModel extends CI_Model
         $zipFileName = 'Boletas.zip';
         $zipFilePath = 'assets/downloads/' . $zipFileName;
         $this->zip->archive($zipFilePath);
+        ini_set('memory_limit', $originalMemoryLimit);
+        gc_collect_cycles();
         return $zipFilePath;
     }
 
