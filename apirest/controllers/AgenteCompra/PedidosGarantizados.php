ea3a174fe030e95fbd8417ce8133197cc655099b
Haruka31Dev <u202412824@upc.edu.pe>
2024-07-17 16:13:57 -0500

    add row autoheigth

--- apirest/controllers/AgenteCompra/PedidosGarantizados.php
+++ apirest/controllers/AgenteCompra/PedidosGarantizados.php
@@ -358,8 +358,6 @@ class PedidosGarantizados extends CI_Controller
             $objPHPExcel->getActiveSheet()->setCellValue("T{$initialRow}", $val->Ss_Costo_Delivery);
             $objPHPExcel->getActiveSheet()->setCellValue("U{$initialRow}", $val->Nu_Dias_Delivery);
             $objPHPExcel->getActiveSheet()->setCellValue("V{$initialRow}", $this->htmlToRichText($this->htmlToTextAndLineBreaks(($val->Txt_Nota))));
-            $objPHPExcel->getActiveSheet()->getRowDimension($initialRow)->setRowHeight(-1);
-
             $initialRow++;
 
         }
@@ -534,7 +532,7 @@ class PedidosGarantizados extends CI_Controller
                 $objPHPExcel->getActiveSheet()->getStyle("Q" . $initialRow)->getNumberFormat()->setFormatCode('0.00');
                 $objPHPExcel->getActiveSheet()->getStyle("S" . $initialRow)->getNumberFormat()->setFormatCode('0.00" KG"');
                 $objPHPExcel->getActiveSheet()->getStyle("T" . $initialRow)->getNumberFormat()->setFormatCode('0.00" KG"');
-                $objPHPExcel->getActiveSheet()->getRowDimension($initialRow)->setRowHeight(-1);
+               
 
                 $initialRow++;
             }
@@ -688,7 +686,6 @@ class PedidosGarantizados extends CI_Controller
             $objPHPExcel->getActiveSheet()->setCellValue("U" . $initialRow, $val->Nu_Dias_Delivery);
             $objPHPExcel->getActiveSheet()->setCellValue("V" . $initialRow, $this->htmlToRichText($this->htmlToTextAndLineBreaks(($val->Txt_Nota))));
             //set auto row height
-            $objPHPExcel->getActiveSheet()->getRowDimension($initialRow)->setRowHeight(-1);
             $initialRow++;
         }
         // $objPHPExcel->getActiveSheet()->setCellValue('T'. ($initialRow),"=SUM(T26:T".($initialRow-1).")");
