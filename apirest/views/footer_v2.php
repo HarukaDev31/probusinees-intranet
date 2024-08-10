dafd2baf4f141d82a3f3f6ecf99a6905825c8ea3
Haruka31uwu <harukakasugano31@gmail.com>
2024-08-10 12:49:05 -0500

    test soccketS

--- apirest/views/footer_v2.php
+++ apirest/views/footer_v2.php
@@ -17,7 +17,7 @@
 
 <!-- jQuery -->
 <script>
-const socket = new WebSocket('ws://localhost:8081');
+const socket = new WebSocket('ws://18.119.139.227:8081');
 
 // Suscribirse a múltiples canales
 function subscribeToChannels(project, role, user) {
