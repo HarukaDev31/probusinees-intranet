5e84ecdbd52e865a37b052c062258ac591c98528
Haruka31Dev <u202412824@upc.edu.pe>
2024-09-20 00:05:26 -0500

    add:socket

--- dist_v2/js/sockets.js
+++ dist_v2/js/sockets.js
@@ -1,1 +1,7 @@
 const socket = new WebSocket('ws://localhost:8081');
+socket.onopen = function(event) {
+    console.log('Connected to the server');
+    // Ejemplo de uso
+    subscribeToChannels('project1', 'admin', 'user123');
+    publishToChannels('project1', 'admin', 'user123', 'Hello everyone!');
+};
