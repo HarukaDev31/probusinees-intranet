69f8f0ea5fa4cde6a0d941daa016e943adff1181
Haruka31Dev <u202412824@upc.edu.pe>
2024-09-20 00:31:53 -0500

    add:socket

--- dist_v2/js/sockets.js
+++ dist_v2/js/sockets.js
@@ -1,7 +1,39 @@
 const socket = new WebSocket('wss://websockets.probusiness.pe');
-socket.onopen = function(event) {
-    console.log('Connected to the server');
-    // Ejemplo de uso
-    subscribeToChannels('project1', 'admin', 'user123');
-    publishToChannels('project1', 'admin', 'user123', 'Hello everyone!');
+// Suscribirse a múltiples canales
+function subscribeToChannels(project, role, user) {
+    const message = JSON.stringify({
+        action: 'subscribe',
+        project: project,
+        role: role,
+        user: user
+    });
+    socket.send(message);
+}
+
+// Publicar un mensaje en múltiples canales
+function publishToChannels(project, role, user, message) {
+    const msg = JSON.stringify({
+        action: 'publish',
+        project: project,
+        role: role,
+        user: user,
+        message: message
+    });
+    socket.send(msg);
+}
+
+// Manejar mensajes recibidos del servidor
+socket.onmessage = function(event) {
+  // console.log(event);
+  //   const message = event.data;
+  //   console.log('Received message:', message);
+  //   // Aquí puedes manejar los mensajes recibidos del servidor
+  //   try{
+  //     alert(message);
+  //   }
+  //   catch(e){
+  //     console.log(e);
+  //   }
+
 };
+subscribeToChannels('project', 'role', 'user');
