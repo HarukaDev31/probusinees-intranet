ab65b6fb2a5234407ba9f4919a954434188dfe36
Francis <ftorres@moventicorp.com>
2024-12-18 10:06:35 -0500

    add fix steps

--- apirest/views/AgenteCompra/PedidosPagadosView.php
+++ apirest/views/AgenteCompra/PedidosPagadosView.php
@@ -795,14 +795,14 @@
     </header>
     <main id="documentation-main">
       <div class="row">
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-md-12 col-lg-6">
           <div>
             <h4>Documentos por subir</h4>
             <div class="documentation-list">
             </div>
           </div>
         </div>
-        <div class="col-12 col-lg-6">
+        <div class="col-12 col-md-12 col-lg-6">
           <div>
                 <header class="sticky top-0 z-10 bg-white border-b">
                 <div class="container mx-auto px-4 py-2 w-100 flex items-center justify-between ">
