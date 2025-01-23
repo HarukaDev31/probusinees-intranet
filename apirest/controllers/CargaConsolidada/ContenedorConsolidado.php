f3d1dccaa04fa095878cf54280806b1628aa8737
Francis <ftorres@moventicorp.com>
2025-01-23 14:36:39 -0500

    changes

--- apirest/controllers/CargaConsolidada/ContenedorConsolidado.php
+++ apirest/controllers/CargaConsolidada/ContenedorConsolidado.php
@@ -593,13 +593,14 @@ class ContenedorConsolidado extends CI_Controller {
 		
 	}
 	public function updateEstadoCotizacionProveedor(){
+		//clean buffer
+		ob_end_clean();
 		$idCotizacion=$this->input->post('idCotizacion');
 		$idProveedor=$this->input->post('idProveedor');
 		$estado=$this->input->post('estado');
 		$arrResponse = $this->ContenedorConsolidadoModel->updateEstadoCotizacionProveedor($idCotizacion,$idProveedor,$estado);
-		echo json_encode([
-			"status" => $arrResponse
-		]);
+		
+		
 	}
 	public function updateTelefonoProveedor(){
 		$idProveedor=$this->input->post('idProveedor');
