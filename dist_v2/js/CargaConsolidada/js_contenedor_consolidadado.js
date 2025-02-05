daaec4da64a9c6dc494b85d921c2135c1ed7308b
Haruka31Dev <u202412824@upc.edu.pe>
2025-02-04 23:30:37 -0500

    fix

--- dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
+++ dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
@@ -1056,7 +1056,7 @@ const openStepFunction = async (step, id) => {
                                             orderable: false,
                                         },
                                     ],
-                                    order: [[1, "asc"]],
+                                   
                                     ajax: {
                                         url: url,
                                         type: "POST",
