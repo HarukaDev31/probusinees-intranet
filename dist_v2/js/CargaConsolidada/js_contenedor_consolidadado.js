1f80b0f21debe1a3cd1821cf873a671bdf3d1246
Haruka31Dev <u202412824@upc.edu.pe>
2025-02-06 17:35:32 -0500

    fix

--- dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
+++ dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
@@ -141,7 +141,7 @@ async function getClientesHeader(){
     const response = await fetch(url);
     const result = await response.json();
     $("#txt-Monto_Total").val(result.monto);
-    $("#txt-CBM_Total_China").val(result.total);
+    $("#txt-CBM_Total_China").val(result.cbm_total_china);
     console.log(result);
     spinner.hide();
     
