ce2674068813e1bb160cb0f1d051e1787adc9e9f
Haruka31Dev <u202412824@upc.edu.pe>
2025-02-27 14:51:55 -0500

    add removed

--- apirest/views/CargaConsolidada/ContenedorConsolidadoView.php
+++ apirest/views/CargaConsolidada/ContenedorConsolidadoView.php
@@ -585,6 +585,42 @@
         </table>
       </div>
   </section>
+  <section class="content card" id="factura-guia-container">
+    <div class="min-h-screen bg-gray-50 p-8">
+      <h1 class="text-3xl font-bold text-gray-800 mb-6">CONSOLIDADO #1: FACTURA Y GUIA</h1>
+      <div class="flex 
+      w-full gap-4 mb-8
+      justify-end
+      
+      ">
+          <!--3 empty divs-->
+       
+        <!--button back-->
+        <button
+          id="btn-back-factura-guia"
+          class="px-6 py-2 bg-white border border-gray-300 text-gray-700 rounded-md hover:bg-gray-50 transition-colors flex items-center gap-2">
+          <div class="fa fa-arrow-left"></div>
+        </button>
+      </div>
+      <div class="table-responsive" class="table table-bordered table-hover table-striped">
+        <table id="table-factura-guia" class="table table-bordered table-hover table-striped">
+          <thead class="thead-light">
+            <tr>
+              <th>N°</th>
+              <th>Nombre</th>
+              <th>DNI/RUC</th>
+              <th>Correo</th>
+              <th>Whatsapp</th>
+              <th>T. Cliente</th>
+              <th>Ajuste</th>
+              <th>C.Final</th>
+              <th>Factura</th>
+              <th>Guia R</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+  </section>
   <div class="modal fade" id="uploadModal" tabindex="-1" aria-labelledby="uploadModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
