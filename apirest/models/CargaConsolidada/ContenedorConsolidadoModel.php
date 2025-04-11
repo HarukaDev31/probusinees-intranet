bbeade627210383d3dfc81ca70af3376f56588ea
HarukaDev31 <harukakasugano31@gmail.com>
2025-04-11 12:28:05 -0500

    fix

--- apirest/models/CargaConsolidada/ContenedorConsolidadoModel.php
+++ apirest/models/CargaConsolidada/ContenedorConsolidadoModel.php
@@ -3246,13 +3246,16 @@ Te avisaré apenas tu carga llegue a nuestro almacén de China, cualquier duda m
     {
         //get sum of cbm_total_china and cbm_total from each cotizacion proveedor
         try {
-            $this->db->select('SUM(ifnull(contenedor_consolidado_cotizacion_proveedores.cbm_total_china,0)) as cbm_total_china,
-                SUM(ifnull(contenedor_consolidado_cotizacion_proveedores.cbm_total,0)) as cbm_total,
-                SUM(IF(contenedor_consolidado_cotizacion.estado_cotizador != "CONFIRMADO", IFNULL(cbm_total, 0), 0)) as cbm_total_pendiente')
+            $this->db->select('
+    SUM(IF(contenedor_consolidado_cotizacion.estado_cotizador = "CONFIRMADO", IFNULL(cbm_total_china, 0), 0)) as cbm_total_china,
+    SUM(IF(contenedor_consolidado_cotizacion.estado_cotizador = "CONFIRMADO", IFNULL(cbm_total, 0), 0)) as cbm_total_confirmado,
+    SUM(IF(contenedor_consolidado_cotizacion.estado_cotizador != "CONFIRMADO", IFNULL(cbm_total, 0), 0)) as cbm_total_pendiente')
+            // $this->db->select('SUM(ifnull(contenedor_consolidado_cotizacion_proveedores.cbm_total_china,0)) as cbm_total_china,
+            //     SUM(ifnull(contenedor_consolidado_cotizacion_proveedores.cbm_total,0)) as cbm_total,
+            //     SUM(IF(contenedor_consolidado_cotizacion.estado_cotizador != "CONFIRMADO", IFNULL(cbm_total, 0), 0)) acbm_totals cbm_total_pendiente')
                 ->from($this->table_contenedor_cotizacion_proveedores)
                 ->join($this->table_contenedor_cotizacion, 'contenedor_consolidado_cotizacion_proveedores.id_cotizacion=contenedor_consolidado_cotizacion.id')
-                ->where('contenedor_consolidado_cotizacion_proveedores.id_contenedor', $idContenedor)
-                ->where('contenedor_consolidado_cotizacion.estado_cotizador', 'CONFIRMADO');
+                ->where('contenedor_consolidado_cotizacion_proveedores.id_contenedor', $idContenedor);
             $query = $this->db->get();
             $result = $query->row();
             if ($this->db->error()['code'] != 0) {
