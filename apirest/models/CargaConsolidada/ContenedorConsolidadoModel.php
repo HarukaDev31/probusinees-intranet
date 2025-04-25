3076dddfdbba58b151a8c754fe684a5f6424646a
Haruka31Dev <u202412824@upc.edu.pe>
2025-04-25 12:34:01 -0500

    fiX

--- apirest/models/CargaConsolidada/ContenedorConsolidadoModel.php
+++ apirest/models/CargaConsolidada/ContenedorConsolidadoModel.php
@@ -13,7 +13,7 @@ class ContenedorConsolidadoModel extends CI_Model
     private $table_cliente = 'entidad';
     private $table_usuario = 'usuario';
     private $table = "carga_consolidada_contenedor";
-    private $defaultHoursContactado = 1 * 60 * 24;
+    private $defaultHoursContactado = 1 ;
     private $defaultHoursInteresado = 1 * 60 * 48;
     private $table_pais = "pais";
     private $table_contenedor_steps = "contenedor_consolidado_order_steps";
