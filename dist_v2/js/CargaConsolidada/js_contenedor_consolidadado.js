4bcfffa35685e112ebdd701a5ed45c1b4b3443ce
HarukaDev31 <harukakasugano31@gmail.com>
2025-05-21 15:09:23 -0500

    add donwload button

--- dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
+++ dist_v2/js/CargaConsolidada/js_contenedor_consolidadado.js
@@ -4756,6 +4756,10 @@ async function viewClientesDocumentacion(id, nombrecliente = null) {
                     <div>
                       <span class="file-name">${decodeURIComponent(facturaComercial.split('_').pop())}</span>
                     </div>
+                    <a href="${facturaComercial}" target="_blank" class="btn btn-primary">
+
+                    <i class="fas fa-download"></i>
+                    </a>
                   `
           : `
                     <div class="py-2">No hay archivo disponible</div>
@@ -4772,7 +4776,9 @@ async function viewClientesDocumentacion(id, nombrecliente = null) {
                     <div>
                       <span class="file-name">${decodeURIComponent(excelConfirmacion.split('_').pop())}</span>
                     </div>
-                  `
+                    <a href="${excelConfirmacion}" target="_blank" class="btn btn-primary">
+                    <i class="fas fa-download"></i>
+                    </a>`
           : `
                     <div class="py-2">No hay archivo disponible</div>
                   `}
