6a41c284a96a7f925b68c5d789a79c245f38d883
HarukaDev31 <harukakasugano31@gmail.com>
2025-06-02 12:17:33 -0500

    fix

--- apirest/models/Curso/PedidosCursoModel.php
+++ apirest/models/Curso/PedidosCursoModel.php
@@ -39,10 +39,9 @@ class PedidosCursoModel extends CI_Model{
     	->join($this->table_departamento, $this->table_departamento . '.ID_Departamento = CLI.ID_Departamento', 'left')
     	->where($this->table . '.ID_Empresa', $this->user->ID_Empresa);
 
-		if(!empty($this->input->post('estado_pago')))
+		if(!empty($this->input->post('estado_pago'))){
 			$this->db->where( $this->table . ".Nu_Estado=", $this->input->post('estado_pago'));
-
-		// $this->db->where("Fe_Emision BETWEEN '" . $this->input->post('Filtro_Fe_Inicio') . " 00:00:00' AND '" . $this->input->post('Filtro_Fe_Fin') . " 23:59:59'");
+		}
 
 		if(isset($this->order)) {
 			$order = $this->order;
