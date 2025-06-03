6d7377ca14bdb874080146095f5a856460c441a2
HarukaDev31 <harukakasugano31@gmail.com>
2025-06-03 16:09:21 -0500

    fix

--- apirest/views/Curso/PedidosCursoView.php
+++ apirest/views/Curso/PedidosCursoView.php
@@ -35,7 +35,7 @@
                                     <div style="width: 200px;">
                                         <input type="text" id="txt-Fe_Inicio_Carga"
                                             class="form-control text-center input-date input-report required"
-                                            value="<?php echo dateNow('month_date_ini_report'); ?>">
+                                            value="<?php echo dateNow('fecha_actual_dmy'); ?>">
                                         <span class="help-block text-danger" id="error"></span>
                                     </div>
                                 </div>
