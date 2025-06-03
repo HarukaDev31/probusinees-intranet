9459fbb7972864109ee63b2ec37fa0ec5de7c1bc
HarukaDev31 <harukakasugano31@gmail.com>
2025-06-03 16:18:14 -0500

    fix

--- apirest/views/Curso/PedidosCursoView.php
+++ apirest/views/Curso/PedidosCursoView.php
@@ -43,8 +43,9 @@
                                     <div class="d-flex" style="width:60%">Fecha Fin</div>
                                     <div style="width: 200px;">
                                         <input type="text" id="txt-Fe_Fin_Carga"
+                                        class="form-control input-date input-report required"
                                         value="<?php echo dateNow('fecha_actual_dmy'); ?>"
-                                            class="form-control input-date input-report required">
+                                            >
                                         <span class="help-block text-danger" id="error"></span>
                                     </div>
                                 </div>
